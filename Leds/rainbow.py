78b8e3b9a74b550a705f40c9755de4c492711496
xenon462 <34947093+xenon462@users.noreply.github.com>
2023-02-23 23:55:17 +0300

    Add files via upload
    Signed-off-by: xenon462 <34947093+xenon462@users.noreply.github.com>

---
+++ Leds/rainbow.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from RPi import GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+GPIO.output(26, True)
+sleep(0.2)
+GPIO.output(26, False)
+GPIO.cleanup()
+
+import time
+from rpi_ws281x import PixelStrip, Color
+
+# LED strip configuration:
+LED_COUNT = 27        # Number of LED pixels.
+LED_PIN = 13          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 1       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel(
+                (int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+while True:
+    rainbowCycle(strip) # rainbow
