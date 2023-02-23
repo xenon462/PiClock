b10c25a1ab819d63e4f98c8f47191d8bfc32a3b1
xenon462 <34947093+xenon462@users.noreply.github.com>
2023-02-24 00:16:12 +0300

    Add files via upload
    Signed-off-by: xenon462 <34947093+xenon462@users.noreply.github.com>

---
+++ Clock/ApiKeys.py
@@ -0,0 +1,5 @@
+googleapi = 'YOUR GOOGLE MAPS API KEY'
+mbapi = 'Mapbox API key'  # Mapbox API key 
+#owmapi = 'owmap'  #OWM
+ccapi = 'Climacell v4'  #Climacell v4
+tsApiKey = 'thingspeak Read API Key'  # thingspeak Read API Key
