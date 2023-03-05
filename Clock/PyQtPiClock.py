0463e24ccf2390c1acc8ddfd1c1a93659098056a
xenon462 <xenon462@mail.ru>
2023-03-05 20:47:36 +0300

    Merge remote-tracking branch 'origin/master'

--- Clock/PyQtPiClock.py
+++ Clock/PyQtPiClock.py
@@ -2108,13 +2108,13 @@ class MyMain(QtWidgets.QWidget):
                 os.popen("sudo /usr/bin/python3  /home/pi/PiClock/Leds/all_leds_off.py")
 
             if event.key() == Qt.Key_UP:  # Key '#'
-                os.popen("sudo pkill -f 'PiClock/Leds'")
                 os.popen("sudo -u pi DISPLAY=:0 /home/pi/osd.sh 1%+")
 
             if event.key() == Qt.Key_DOWN:  # Key '#'
-                os.popen("sudo pkill -f 'PiClock/Leds'")
                 os.popen("sudo -u pi DISPLAY=:0 /home/pi/osd.sh 1%-")
 
+            if event.key() == Qt.Key_OK:  # Key '#'
+                os.popen("sudo -u pi DISPLAY=:0 /home/pi/osd.sh 100%-")
 
 
             if event.key() == Qt.Key_1:  # Key '#'
