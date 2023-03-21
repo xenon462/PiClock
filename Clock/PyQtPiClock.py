65415a915a23388ef7c2c58c79da841ee284e937
xenon462 <xenon462@mail.ru>
2023-03-21 11:13:57 +0300

    Merge remote-tracking branch 'origin/master'

--- Clock/PyQtPiClock.py
+++ Clock/PyQtPiClock.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3                        # Climacell
+#!/usr/bin/python3                        # Climacell
 # -*- coding: utf-8 -*-
 
 import requests
--- Leds/NeoAmbi.py
+++ Leds/NeoAmbi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import time
 from rpi_ws281x import PixelStrip, Color
 from time import sleep
