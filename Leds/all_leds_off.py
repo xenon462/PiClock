8b6f78211ebfc6c1178b9d17c25467df65779231
xenon462 <xenon462@mail.ru>
2023-04-10 20:44:10 +0300

    Merge remote-tracking branch 'origin/master'

--- Leds/all_leds_off.py
+++ Leds/all_leds_off.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from RPi import GPIO
 from time import sleep
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 GPIO.output(26, True)
@@ -9,25 +10,27 @@ GPIO.output(26, False)
 GPIO.cleanup()
 import os
 
-os.popen("sudo pkill -f '/home/pi/PiClock/Leds/'") # убить все процессы из папки Leds
+os.popen("sudo pkill -f '/home/pi/PiClock/Leds/'")  # убить все процессы из папки Leds
 
 import subprocess
+
 proc = subprocess.Popen(["pkill", "-f", "/home/pi/PiClock/Leds/rainbow.py"], stdout=subprocess.PIPE)
 proc.wait()
 
 import time
 from rpi_ws281x import PixelStrip, Color
-#import argparse
+
+# import argparse
 
 # LED strip configuration:
-LED_COUNT = 27        # Number of LED pixels.
-LED_PIN = 13          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_COUNT = 34  # Number of LED pixels.
+LED_PIN = 13  # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_DMA = 10  # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 1       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 1  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 # Define functions which animate LEDs in various ways.
@@ -38,11 +41,11 @@ def colorWipe(strip, color, wait_ms=50):
         strip.show()
         time.sleep(wait_ms / 1000.0)
 
+
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
 while True:
     colorWipe(strip, Color(0, 0, 0))  # Off
-    quit()    
-
+    quit()
