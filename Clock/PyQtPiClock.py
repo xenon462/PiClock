b03cfa20c6bd422193639193c47c5cd06973b1e0
xenon462 <xenon462@mail.ru>
2023-10-25 09:23:37 +0300

    Merge remote-tracking branch 'origin/master'

--- Clock/PyQtPiClock.py
+++ Clock/PyQtPiClock.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3                        # Climacell
+#!/usr/bin/python3                        # Tomorrow.io
 # -*- coding: utf-8 -*-
-
 import datetime
 import json
 import locale
